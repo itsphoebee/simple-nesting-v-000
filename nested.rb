20f077e8cd1f97d4a0fa12c971900df2e4a86e3d
Phoebe Lau <phoebelau7@gmail.com>
2017-12-01 02:51:49 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -61,19 +61,31 @@ programmer_hash[:dennis_ritchie][:languages].join
 
 end
 
-def adding_matz(programmer_hash)
+def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
 
+	programmer_hash =
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
 
+programmer_hash[:yukihiro_matsumoto] = {}
 
-programmer_hash[:yukihiro_matsumoto] = {
-   :known_for => "Ruby",
-   :languages => ["LISP", "C"]
-}
 
 end
 
