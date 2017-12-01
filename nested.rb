a440aa5b7dd1f27c080125aa635a34902fc28fa8
Phoebe Lau <phoebelau7@gmail.com>
2017-12-01 03:09:05 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,11 @@ def adding_matz
         }
      }
 
-#programmer_hash[:yukihiro_matsumoto] =
+programmer_hash.merge!(:yukihiro_matsumoto => {
+   :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
+    )
 
 
 end
@@ -108,7 +112,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 
-programmer_hash[:alan_kay][0] = alans_new_info
+programmer_hash[:alan_kay][:known_for] = alans_new_info
 end
 
 def adding_to_dennis
